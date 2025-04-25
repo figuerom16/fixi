d291e49c2f61d85ff2bb68f38310b99fb109abb1
figuerom16 <figuerom16@gmail.com>
2025-04-25 12:34:12 -0700

    revert regular fixi

--- fixi.js
+++ fixi.js
@@ -1,5 +1,5 @@
-(_=>{
-	if(document.__fixi_mo) return
+(()=>{
+	if(document.__fixi_mo) return;
 	document.__fixi_mo = new MutationObserver((recs)=>recs.forEach((r)=>r.type === "childList" && r.addedNodes.forEach((n)=>process(n))))
 	let send = (elt, type, detail, bub)=>elt.dispatchEvent(new CustomEvent("fx:" + type, {detail, cancelable:true, bubbles:bub !== false, composed:true}))
 	let attr = (elt, name, defaultVal)=>elt.getAttribute(name) || defaultVal
@@ -9,26 +9,19 @@
 		if (elt.__fixi || ignore(elt) || !send(elt, "init", {options})) return
 		elt.__fixi = async(evt)=>{
 			let reqs = elt.__fixi.requests ||= new Set()
-			let form = elt.form || elt.closest("form") || elt.closest("tr")
+			let form = elt.form || elt.closest("form")
 			let body = new FormData(form ?? undefined, evt.submitter)
 			if (!form && elt.name) body.append(elt.name, elt.value)
-			if (form.tagName == 'TR') {
-				for (const cell of row.cells){
-					const name = cell.getAttribute('name')
-					if(name) e.detail.cfg.body.append(name, cell.innerText.trim())
-				}
-			}
-			if (!['file','image'].includes(elt.type) && !form?.querySelector('input[type="file"], input[type="image"]')) body = new URLSearchParams(body)
 			let ac = new AbortController()
 			let cfg = {
 				trigger:evt,
-				action:attr(elt, "fx-action", ""),
-				method:attr(elt, "fx-method")?.toUpperCase(),
+				action:attr(elt, "fx-action"),
+				method:attr(elt, "fx-method", "GET").toUpperCase(),
 				target:document.querySelector(attr(elt, "fx-target")) ?? elt,
-				swap:attr(elt, "fx-swap", "innerHTML"),
+				swap:attr(elt, "fx-swap", "outerHTML"),
 				body,
 				drop:reqs.size,
-				headers:{"FX-Tag":elt.tagName,"FX-Id":elt.id},
+				headers:{"FX-Request":"true"},
 				abort:ac.abort.bind(ac),
 				signal:ac.signal,
 				preventTrigger:true,
@@ -39,7 +32,9 @@
 			if (cfg.preventTrigger) evt.preventDefault()
 			if (!go || cfg.drop) return
 			if (/GET|DELETE/.test(cfg.method)){
-				if (cfg.body.size) cfg.action += (/\?/.test(cfg.action) ? "&" : "?") + cfg.body
+				let params = new URLSearchParams(cfg.body)
+				if (params.size)
+					cfg.action += (/\?/.test(cfg.action) ? "&" : "?") + params
 				cfg.body = null
 			}
 			reqs.add(cfg)
@@ -59,27 +54,32 @@
 				reqs.delete(cfg)
 				send(elt, "finally", {cfg})
 			}
-			let doSwap = _=>{
-				if (cfg.swap instanceof Function) return cfg.swap(cfg)
-				else if (/(before|after)(begin|end)/.test(cfg.swap)) cfg.target.insertAdjacentHTML(cfg.swap, cfg.text)
-				else if(cfg.swap in cfg.target) cfg.target[cfg.swap] = cfg.text
+			let doSwap = ()=>{
+				if (cfg.swap instanceof Function)
+					return cfg.swap(cfg)
+				else if (/(before|after)(begin|end)/.test(cfg.swap))
+					cfg.target.insertAdjacentHTML(cfg.swap, cfg.text)
+				else if(cfg.swap in cfg.target)
+					cfg.target[cfg.swap] = cfg.text
 				else throw cfg.swap
 			}
-			if (cfg.transition) await cfg.transition(doSwap).finished
-			else await doSwap()
+			if (cfg.transition)
+				await cfg.transition(doSwap).finished
+			else
+				await doSwap()
 			send(elt, "swapped", {cfg})
 			if (!document.contains(elt)) send(document, "swapped", {cfg})
 		}
-		elt.__fixi.evt = attr(elt, "fx-trigger", elt.matches("form") ? "submit" : elt.matches("input:not([type=button]),select,textarea") ? "change" : "click").split("|")
-		elt.__fixi.evt.forEach(a=>{elt.addEventListener(a, elt.__fixi, options)})
+		elt.__fixi.evt = attr(elt, "fx-trigger", elt.matches("form") ? "submit" : elt.matches("input:not([type=button]),select,textarea") ? "change" : "click")
+		elt.addEventListener(elt.__fixi.evt, elt.__fixi, options)
 		send(elt, "inited", {}, false)
 	}
 	let process = (n)=>{
 		if (n.matches){
 			if (ignore(n)) return
-			if (n.matches("[fx-method]")) init(n)
+			if (n.matches("[fx-action]")) init(n)
 		}
-		if(n.querySelectorAll) n.querySelectorAll("[fx-method]").forEach(init)
+		if(n.querySelectorAll) n.querySelectorAll("[fx-action]").forEach(init)
 	}
 	document.addEventListener("fx:process", (evt)=>process(evt.target))
 	document.addEventListener("DOMContentLoaded", ()=>{
@@ -87,91 +87,3 @@
 		process(document.body)
 	})
 })()
-
-document.addEventListener('fx:init',e=>{//Disable During Request
-	const el = e.target
-	if(!el.matches('[fx-disable]')) return
-	const disableSelector = el.getAttribute('fx-disable')
-	el.addEventListener('fx:before',_=>{
-		let disableTarget = disableSelector == "" ? el : document.querySelector(disableSelector)
-		disableTarget.disabled = true
-		el.addEventListener('fx:after', (afterEvt)=>{if(afterEvt.target == el) disableTarget.disabled = false})
-	})
-})
-
-document.addEventListener('fx:config',e=>{//Confirm Dialog
-	const confirmationMessage = e.target.getAttribute('fx-confirm')
-	if(confirmationMessage) e.detail.cfg.confirm =_=>confirm(confirmationMessage)
-})
-
-document.addEventListener('fx:init',e=>{//Debounce
-	let target = e.target
-	if (!target.hasAttribute('fx-debounce')) return
-	target.addEventListener('fx:inited', _=>{
-		target.removeEventListener(target.__fixi.evt, target.__fixi)
-		let debounceTime = parseInt(target.getAttribute('fx-debounce'))
-		let timeout = null
-		target.addEventListener(target.__fixi.evt,e=>{
-			clearTimeout(timeout)
-			timeout = setTimeout(_=>target.__fixi(e), debounceTime)
-		})
-	})
-})
-
-document.addEventListener('fx:config',e=>{//Relative Selectors
-	const target = e.target.getAttribute('fx-target') || ""
-	if(target.indexOf('closest ') == 0) e.detail.cfg.target = e.target.closest(target.substring(8))
-	else if(target.indexOf('find ') == 0) e.detail.cfg.target = e.target.closest(target.substring(5))
-	else if(target.indexOf('next ') == 0){
-		const matches = Array.from(document.querySelectorAll(target.substring(5)))
-		e.detail.cfg.target = matches.find((el)=>e.target.compareDocumentPosition(el) === Node.DOCUMENT_POSITION_FOLLOWING)
-	} else if(target.indexOf('previous ') == 0){
-		const matches = Array.from(document.querySelectorAll(target.substring(9))).reverse()
-		e.detail.cfg.target = matches.find((el)=>e.target.compareDocumentPosition(el) === Node.DOCUMENT_POSITION_PRECEDING)
-	}
-})
-
-document.addEventListener('fx:config',e=>{//Vals
-	if(!e.target.matches('[fx-vals]')) return
-	const valsAttr = e.target.getAttribute('fx-vals')
-	let vals
-	if(valsAttr.startsWith('js:')) vals = new Function('return ' + valsAttr.slice(3))()
-	else vals = new Function('return ' + valsAttr)()
-	if(typeof vals !== 'object' || vals === null || Array.isArray(vals)){
-		console.error('fx-vals not a valid object:', vals);return
-	}
-	for (let key in vals){
-		if(typeof key === 'string' && key.trim() === '') continue
-		e.detail.cfg.body.append(key, vals[key])
-	}
-})
-
-document.addEventListener('fx:before',_=>{//Clear Error & Success
-	me('#error').textContent = me('#success').textContent = ''
-})
-
-document.addEventListener('fx:after',e=>{//Set Error & Success
-	if(e.detail.cfg.response.status < 400) setTimeout(_=>{me('#success').textContent = ''}, 2000)
-	else {e.detail.cfg.target = me('#error');e.detail.cfg.swap = 'innerHTML'}
-})
-
-document.addEventListener('fx:finally',e=>{//Refresh
-	if(!e.target.matches('[fx-refresh]')) return
-	document.location.reload()
-})
-
-document.addEventListener('fx:swapped',e=>{//Run Scripts then Create Icons
-	e.detail.cfg.target.querySelectorAll('script').forEach(s=>
-		s.replaceWith(Object.assign(document.createElement('script'),{textContent:s.textContent}))
-	)
-	if(lucide) lucide.createIcons()
-})
-
-document.addEventListener('mousedown',e=>{//fclick
-	if(e.button || !e.target.closest('[fclick]')) return
-	e.preventDefault();e.target.click()
-})
-document.addEventListener('touchstart',e=>{
-	if(!e.target.closest('[fclick]')) return
-	e.preventDefault();e.target.click()
-})
