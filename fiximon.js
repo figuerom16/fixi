96fb751d800e4328a57221c8b056574802e9c64c
figuerom16 <figuerom16@gmail.com>
2025-04-26 11:52:46 -0700

    make nav a method

--- fiximon.js
+++ fiximon.js
@@ -192,30 +192,19 @@ document.addEventListener('touchstart',e=>{
 // COMMON
 function $(s) { // s=selector, el=element, els=elements
 	let el, els
-	const start = document.currentScript
-	if (!s) el = start.parentElement
+	const start = document.currentScript // Doesn't work in callback use Event or QuerySelector
+	if (!s) el = start.parentElement ?? console.warn('$(): Fails inside callback.')
 	else if (s instanceof Event) el = s.currentTarget ?? console.warn('$: Event is Null')
 	else if (typeof s !== 'string') {console.warn('$: Not a String'); return null}
-	else if (s == '-') el = start.previousElementSibling
+	else if (s == '-') el = start.previousElementSibling ?? console.warn('$(\'-\'): Fails inside callback.')
 	else if (s.indexOf('closest ') == 0) el = start.closest(s.substring(8))
-	else if (s.indexOf('next ') == 0) {
+	else if (s.indexOf('next ') == 0){
 		const matches = Array.from(document.querySelectorAll(s.substring(5)))
 		el = matches.find((el)=>start.compareDocumentPosition(el) === Node.DOCUMENT_POSITION_FOLLOWING)
-	} else if (s.indexOf('previous ') == 0) {
+	}
+	else if (s.indexOf('previous ') == 0){
 		const matches = Array.from(document.querySelectorAll(s.substring(9))).reverse()
 		el = matches.find((el)=>start.compareDocumentPosition(el) === Node.DOCUMENT_POSITION_PRECEDING)
-	} else if (s.indexOf('nav ') == 0) {
-		el = start
-		for (const n of s.substring(4).split(' ')) {
-			switch (n) {
-				case 'parent': el = el.parentElement; break
-				case 'next': el = el.nextElementSibling; break
-				case 'prev': el = el.previousElementSibling; break
-				case 'first': el = el.firstElementChild; break
-				case 'last': el = el.lastElementChild; break
-				default: console.warn(`$: Nav ${n} is not Valid`)
-			}
-		}
 	}
 	if (el) els = [el]
 	else {
@@ -225,6 +214,21 @@ function $(s) { // s=selector, el=element, els=elements
 	return { // e=event, c=callback
 		$: els[0],
 		all: els,
+		nav: (nav)=>{
+			el = els[0]
+			for (const n of nav.split(' ')) {
+				switch (n) {
+					case 'parent': el = el.parentElement; break
+					case 'next': el = el.nextElementSibling; break
+					case 'previous': el = el.previousElementSibling; break
+					case 'first': el = el.firstElementChild; break
+					case 'last': el = el.lastElementChild; break
+					default: console.warn(`$: Nav ${n} is not Valid`)
+				}
+			}
+			return el
+		},
+		// Add more returns here
 		on: (e, c)=>(els.forEach(el => el.addEventListener(e, c)), this),
 		off: (e, c)=>(els.forEach(el => el.removeEventListener(e, c)), this),
 		run: (c)=>(els.forEach(_=>f(c)), this),
