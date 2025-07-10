a6c78102b68bf83f59eea35b23a1a5e589c8a819
figuerom16 <figuerom16@gmail.com>
2025-07-10 09:55:08 -0700

    add in none swap from fixi

--- fiximon.js
+++ fiximon.js
@@ -141,7 +141,7 @@ function signal(init) {
 				if (cfg.swap instanceof Function) return cfg.swap(cfg)
 				else if (/(before|after)(begin|end)/.test(cfg.swap)) cfg.target.insertAdjacentHTML(cfg.swap, cfg.text)
 				else if(cfg.swap in cfg.target) cfg.target[cfg.swap] = cfg.text
-				else throw cfg.swap
+				else if(cfg.swap !== 'none') throw cfg.swap
 			}
 			if (cfg.transition) await cfg.transition(doSwap).finished
 			else await doSwap()
