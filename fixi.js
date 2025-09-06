1e7929b34a630ed054ce9978c7f4c93c096a0355
figuerom16 <figuerom16@gmail.com>
2025-09-06 10:54:34 -0700

    Undo again. not working

--- fixi.js
+++ fixi.js
@@ -70,7 +70,7 @@
 			send(elt, "swapped", {cfg})
 			if (!document.contains(elt)) send(document, "swapped", {cfg})
 		}
-		elt.__fixi.evt = attr(elt, "fx-trigger", elt.matches("form,button:not([type=button]):not([type=reset]),input:is([type=submit],[type=image]") ? "submit" : elt.matches("input:not([type=button]),select,textarea") ? "change" : "click")
+		elt.__fixi.evt = attr(elt, "fx-trigger", elt.matches("form,button:form not([type=button]):not([type=reset]),form input:is([type=submit],[type=image]") ? "submit" : elt.matches("input:not([type=button]),select,textarea") ? "change" : "click")
 		elt.addEventListener(elt.__fixi.evt, elt.__fixi, options)
 		send(elt, "inited", {}, false)
 	}
