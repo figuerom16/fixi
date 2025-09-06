9e2ec8f85eaf1f01f5ca8b5deff644b2f7972b07
figuerom16 <figuerom16@gmail.com>
2025-09-06 12:01:24 -0700

    remove uneeded css selectors

--- fiximon.js
+++ fiximon.js
@@ -148,7 +148,7 @@ function signal(init) {
 			send(elt, "swapped", {cfg})
 			if (!document.contains(elt)) send(document, "swapped", {cfg})
 		}
-		elt.__fixi.evt = attr(elt, "fx-trigger", elt.matches("form,form button:not([type=button]):not([type=reset]),form input:is([type=submit],[type=image]") ? "submit" : elt.matches("input:not([type=button]),select,textarea") ? "change" : "click").split("|")
+		elt.__fixi.evt = attr(elt, "fx-trigger", elt.matches("form") ? "submit" : elt.matches("input:not([type=button]),select,textarea") ? "change" : "click").split("|")
 		elt.__fixi.evt.forEach(a=>{elt.addEventListener(a, elt.__fixi, options)})
 		send(elt, "inited", {}, false)
 	}
